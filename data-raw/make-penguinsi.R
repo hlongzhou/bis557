c8499da20fea910b2acafe140abc4445b0568ef1
Tianqi Chen <tianqi.chen.7@gmail.com>
2020-09-08 00:30:36 -0400

    Comment out tidy version of penguins.

--- data-raw/make-penguinsi.R
+++ data-raw/make-penguinsi.R
@@ -5,12 +5,12 @@ library(missForest)
 penguinsi <- penguins
 penguinsi <- missForest(as.data.frame(penguinsi))
 
-# Tidy
-library(dplyr)
-penguinsi <- penguins %>%
-  as.data.frame() %>%
-  missForest() %>%
-  `$`(ximp) %>%
-  as_tibble()
+# Tidy version (for reference)
+# library(dplyr)
+# penguinsi <- penguins %>%
+#   as.data.frame() %>%
+#   missForest() %>%
+#   `$`(ximp) %>%
+#   as_tibble()
 
 use_data(penguinsi) # create a data folder, and produce processed data.
