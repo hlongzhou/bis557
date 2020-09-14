ce6ccf3dca3656f1e658959efe9c24ec8501aa02
Tianqi Chen <tianqi.chen.7@gmail.com>
2020-09-14 16:11:00 -0400

    Updated function and added test.

--- R/grab_coeffs.r
+++ R/grab_coeffs.r
@@ -2,7 +2,8 @@
 #' @description This is a simple function to grab the slope coefficients of a
 #' linear model.
 #' @param lm_obj the linear model object created by the lm() function
-#' @example
+#'
+#' @examples
 #' library(palmerpenguins)
 #' fit <- lm(bill_length_mm ~ ., data = penguins[, -8])
 #' grab_coeffs(fit)
--- R/grab_resids.r
+++ R/grab_resids.r
@@ -2,11 +2,12 @@
 #' @description This is a simple function to grab the residuals of a
 #' linear model.
 #' @param lm_obj the linear model object created by the lm() function
-#' @example
+
+#' @examples
 #' library(palmerpenguins)
 #' fit <- lm(bill_length_mm ~ ., data = penguins[, -8])
 #' grab_resids(fit)
 #' @export
 grab_resids <- function(lm_obj){
-  lm_obj$residual
+  lm_obj$residuals
 }
